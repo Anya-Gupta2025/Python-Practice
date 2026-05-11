509276facaf23ed9f181500895bf1e354a7db190
Anya-Gupta2025 <guptaan@wsc.school.nz>
2026-05-11 15:06:55 +1200

    Made some progress on the Quiz
    Finished making the questions and writing their answers

--- quiz.py
+++ quiz.py
@@ -5,4 +5,37 @@ import textwrap
 import random
 
 # Constant variabes which wouldn't change thoughout the programme
-INSTRUCTIONS = "Welcome to Magic Quiz Madness. Play and answer the questions if you dare... You might be left with some fascinating, mysterious powers by the end of this!"
+INSTRUCTIONS = "Welcome to Magic Quiz Madness. Play and answer the questions if you dare... You might be left with some fascinating, mysterious powers by the end of this! This "
+
+QUESTION_ONE = "How many days are in a leap year?"
+ANSWER_ONE = 366
+
+QUESTION_TWO = "Which animal is known as the “King of the Jungle”?"
+ANSWER_TWO = "lion"
+
+QUESTION_THREE = "What is the capital city of Japan?"
+ANSWER_THREE = "Tokyo"
+
+QUESTION_FOUR = "Which organ pumps blood around the human body?"
+ANSWER_FOUR = "heart"
+
+QUESTION_FIVE = "What is the fastest land animal?"
+ANSWER_FIVE = "cheetah"
+
+QUESTION_SIX = "Which scientist developed the theory of gravity after observing a falling apple?" 
+ANSWER_SIX = "Isaac Newton"
+
+QUESTION_SEVEN = "In mathematics, what is the value of π (pi) rounded to 2 decimal places?"
+ANSWER_SEVEN = 3.14
+
+QUESTION_EIGHT = "Which country has the largest population in the world as of recent years?"
+ANSWER_EIGHT = "India"
+
+QUESTION_NINE = "What is the name of the deepest known part of the world’s oceans?"
+ANSWER_NINE = "Mariana Trench"
+
+QUESTION_TEN = "How tall is the tallest human ever born on Earth (your guess will be tested against other people's guesses and you will get an email if you win this quiz!)?"
+ANSWER_TEN = {"meters": 2.72, "centimeters": 272}
+
+
+# Question 1:
