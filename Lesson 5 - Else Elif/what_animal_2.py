374bfeb122ef27151a70783ff22bf20e11e99769
Anya-Gupta2025 <guptaan@wsc.school.nz>
2026-05-19 14:38:38 +1200

    Finished Quiz Extension

---
+++ Lesson 5 - Else Elif/what_animal_2.py
@@ -0,0 +1,103 @@
+### WHAT ANIMAL ARE YOU QUIZ ###
+# EXTENSION 2
+# Create a 'Which ??? are you?' Quiz
+# This time allow all questions to have 4 possible answers (a,b,c and d) 
+# and tally how many times they choose each
+# Determine what they are at the end using the letter with the highest tally.
+# Eg. If they picked mostly As, maybe they are Pikachu.
+
+# Some variables:
+a_score = 0
+b_score = 0
+c_score = 0
+d_score = 0
+response = False
+
+# Function to tally up the points:
+def tally_points(a):
+    # Making variables global so that this function can modify them:
+    global a_score, b_score, c_score, d_score, response
+
+    while not response:
+        if a == "a":
+            a_score += 1
+            response = True
+
+        elif a == "b":
+            b_score += 1 
+            response = True
+
+        elif a == "c":
+            c_score += 1 
+            response = True
+
+        elif a == "d":
+            d_score += 1
+            response = True
+
+        else:
+            print("Invalid answer, try again.")
+            a = input("Answer: ").lower() 
+
+
+# Give instructions to the user
+print("Hello! Wlecome to this quiz! Answer the next few questions in order to find out what animal you are! \n")
+
+
+## 3 questions and 4 animals ##
+
+# Ask your user a question about themselves, giving them 4 options
+
+print("What kind of person are you?")
+print(" a. Morning \n b. Afternoon \n c. Evening \n d. Night \n")
+
+response_1 = input("Answer: ").lower()
+print()
+
+# Tally their score
+tally_points(response_1)
+
+
+# Ask the next question
+print("What animal do you most find to most reasonate with you?")
+print(" a. Fish \n b. Cat \n c. Dog \n d. Bird \n")
+
+response_2 = input("Answer: ").lower()
+print()
+
+# Tally their score
+tally_points(response_2)
+
+
+#Ask the next question:
+
+print("If you could choose a colour, what colour would you be?")
+print(" a. Green \n b. Red \n c. Blue \n d. Yellow \n")
+
+response_3 = input("Answer: ").lower()
+print()
+
+# Tally thier score
+tally_points(response_3)
+
+
+# Tell them their animal:
+if a_score < 2 and b_score < 2 and c_score < 2 and d_score < 2:
+    print("You are a Butterfly.")
+
+else:
+    greatest = max(a_score, b_score, c_score, d_score)
+
+    print()
+
+    if greatest == a_score:
+        print("You are a Shark.")
+
+    elif greatest == b_score:
+        print("You are a Lion.") 
+
+    elif greatest == c_score:
+        print("You are an Owl.") 
+
+    else:
+        print("You are a Duckling!")
