ab875dc74a90c21e82599c216a505cf232b717c0
Anya-Gupta2025 <guptaan@wsc.school.nz>
2026-06-21 03:46:57 +1200

    Fixed the debugging project from lesson 12

--- Lesson 12 - Collections/debugging_13.py
+++ Lesson 12 - Collections/debugging_13.py
@@ -1,3 +1,4 @@
+# List of heroes
 hero_data = {
     "Player1": {
         "class": "Warrior", 
@@ -10,17 +11,28 @@ hero_data = {
         "gear": ["staff"]}
 }
 
+# Print hero data
 print("\nCurrent Players: ")
 
+# Run through the list of heroes
 for i in range(len(hero_data)):
-    print(f"Player {i+1}")
+    print(f"\nPlayer {i+1}")
 
-    if hero_data{"Player1"}{"class"} == "Warrior":
-        print("Player 1 is a frontline fighter.")
+    if hero_data["Player"+str(i+1)]["class"] == "Warrior":
+        print(f"Player {i+1} is a frontline fighter.")
 
-    starting_weapon = hero_data["Player1"]["gear"[0]]
-print(f"Starting weapon for Player 1: {starting_weapon}")
-hero_data["Player2"]{"health"} = 75
-print(f"Updated Player 2 Data: {hero_data["Player2"]}")
+    starting_weapon = hero_data["Player"+str(i+1)]["gear"][0]
+
+    # Print the starting weapon for each player
+    print(f"Starting weapon for Player {i+1}: {starting_weapon}")
+
+# Update hero 2
+hero_data["Player2"]["health"] = 75
+
+# Print updated hero two
+print(f"Updated Player 2 Data: {hero_data['Player2']}")
+
 # IF the string "staff" is IN the list stored at hero_data "Player2" "gear"
 # THEN print "Mage is fully equipped"
+if "staff" in hero_data["Player2"]["gear"]:
+    print("Mage is fully equipped\n")
