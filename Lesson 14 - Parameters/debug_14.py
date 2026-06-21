e7d58316b0f7e1869b450a5cf13af2d45ea093a3
Anya-Gupta2025 <guptaan@wsc.school.nz>
2026-06-22 11:57:29 +1200

    Finished 14 debug practice

---
+++ Lesson 14 - Parameters/debug_14.py
@@ -0,0 +1,21 @@
+# Find calories burnt
+def burn_calories(current_total, activity_mins):
+    calories_burned = activity_mins * 8.5
+    new_total = current_total - calories_burned
+    return new_total
+
+# Find total calories
+def log_meal(base_calories, meal_weight):
+    total_calories = base_calories + meal_weight
+    return total_calories
+
+# Main code
+def main():
+    daily_calories = 2000.0
+    log_meal(daily_calories, 450.0)
+    print(f"Calories after breakfast: {daily_calories}")
+    remaining_calories = burn_calories(daily_calories, 30)
+    print(f"Remaining calories after workout: {remaining_calories}")
+
+# Calling the main function
+main()
